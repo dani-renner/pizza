e987966f9894435fda9c13bb5bab5641afc07472
Dani Renner <danijrenner@gmail.com>
2021-01-29 10:47:49 -0800

    not functioning yet: pass input into object

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 function Pizza (size) {
   this.size = size;
-  this.toppings = {};
+  this.toppings = [];
 }
+Pizza.prototype.countToppings = function() {
+  return Object.keys().length;
+}
+$(document).ready(function() {
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+    const inputtedSize = $("input:radio[name=size]:checked").val();
+    const inputtedToppings = $("input:checkbox[name=toppings]:checked").toArray();
+    let userPizza = new Pizza(inputtedSize,inputtedToppings);
+    console.log(userPizza.toppings);
+    });
+    
+  });
